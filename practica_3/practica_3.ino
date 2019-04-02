184915e4e438d4d9cc245a185fba03f566c5029f
Hegar Garcia <hegargarcia@gmail.com>
2019-04-02 17:53:33 -0600

    Make code smaller

--- practica_3/practica_3.ino
+++ practica_3/practica_3.ino
@@ -1,22 +1,20 @@
 void setup() {
-  DDRB = DDRC | 0X31, DDRD = DDRD | 0xF3;
   attachInterrupt(digitalPinToInterrupt(2), setGreen, RISING);
   attachInterrupt(digitalPinToInterrupt(3), setRed, RISING);
-  setColor(255, 255, 0);
+  setColor(255, 35);
 }
 
 void loop() {}
 
 void setGreen() {
-  setColor(0, 255, 0);
+  setColor(0, 255);
 }
 
 void setRed() {
-  setColor(255, 0, 0);
+  setColor(255, 0);
 }
 
-void setColor(int red, int green, int blue) {
+void setColor(int red, int green) {
   analogWrite(11, red);
   analogWrite(10, green);
-  analogWrite(9, blue);
 }
